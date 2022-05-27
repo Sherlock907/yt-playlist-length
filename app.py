e5999aef3a1d32225440b1c74f23621fe430350d
Sherlock907 <106328395+Sherlock907@users.noreply.github.com>
2022-05-27 16:24:01 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request,flash, Markup
 from pytube import Playlist, YouTube
 
+
 def secondsTo(n):
     n = int(n)
     if n < 60:
@@ -26,6 +27,15 @@ def index():
                 flash("Sie suchen also nach dem großen Lebautzski")
                 flash(Markup("Hier werden Sie fündig:  <a href='https://youtu.be/9cK3QYZ7XaA'>The Big Lebautzski</a>"))
                 return render_template('index.html')
+            elif x == "mariya":
+                flash("Thank you for your insightful tutorials, Mariya.")
+                return render_template('index.html')
+            elif sorted(x) == sorted("mariya"):
+                flash("Having a hard time writing your own name? - No Problem.")
+                flash(Markup('<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>'))
+                flash(Markup('''<h1 class="ml2">It's Mariya</h1>'''))
+                flash(Markup('''<script>var textWrapper=document.querySelector('.ml2');textWrapper.innerHTML=textWrapper.textContent.replace(/\S/g,"<span class='letter'>$&</span>");anime.timeline({loop:true}).add({targets:'.ml2 .letter',opacity:[0,0],translateZ:0,easing:"easeOutExpo",duration:1750,delay:(el,i)=>70*i}).add({targets:'.ml2 .letter',scale:[4,1],opacity:[0,1],translateZ:0,easing:"easeOutExpo",duration:2550,delay:(el,i)=>700*i}).add({targets:'.ml2',opacity:0,duration:1000,easing:"easeOutExpo",delay:1000});</script>'''))
+                return render_template('yt-playlist.html')
             p = Playlist(request.form['yt-link'])
             playlist_duration = 0
 
@@ -49,4 +59,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(port=1337, debug=False)
+    app.run(port=1337, debug=True)
