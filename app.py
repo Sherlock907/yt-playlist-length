26fd529656c9160306dede9c5c9843f856e24e9b
Sherlock907 <106328395+Sherlock907@users.noreply.github.com>
2022-05-27 18:01:24 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -28,24 +28,24 @@ def index():
                 flash(Markup("Hier werden Sie fündig:  <a href='https://youtu.be/9cK3QYZ7XaA'>The Big Lebautzski</a>"))
                 return render_template('index.html')
             elif x == "mariya":
-                flash("Thank you for your insightful tutorials, Mariya.")
+                flash(Markup("<h5 style='padding-bottom: 15px;text-decoration:underline;'>Thank you for your insightful tutorials, Mariya.</h5>"))
                 return render_template('index.html')
             elif sorted(x) == sorted("mariya"):
                 flash("Having a hard time writing your own name? - No Problem.")
                 flash(Markup('<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>'))
                 flash(Markup('''<h1 class="ml2">It's Mariya</h1>'''))
                 flash(Markup('''<script>var textWrapper=document.querySelector('.ml2');textWrapper.innerHTML=textWrapper.textContent.replace(/\S/g,"<span class='letter'>$&</span>");anime.timeline({loop:true}).add({targets:'.ml2 .letter',opacity:[0,0],translateZ:0,easing:"easeOutExpo",duration:1750,delay:(el,i)=>70*i}).add({targets:'.ml2 .letter',scale:[4,1],opacity:[0,1],translateZ:0,easing:"easeOutExpo",duration:2550,delay:(el,i)=>700*i}).add({targets:'.ml2',opacity:0,duration:1000,easing:"easeOutExpo",delay:1000});</script>'''))
-                return render_template('yt-playlist.html')
+                return render_template('index.html')
             p = Playlist(request.form['yt-link'])
             playlist_duration = 0
 
 
-            flash(Markup("Playlist: <a href='https://www.youtube.com/playlist?list=" + p.playlist_id + "'>" + p.title + "</a>"))
+            flash(Markup("<a href='https://www.youtube.com/playlist?list=" + p.playlist_id + "'>" + p.title + "</a>"))
             for url in p:
                 playlist_duration += YouTube(url).length
             flash("Total Playlist duration:\n" + secondsTo(playlist_duration))
-            flash("Duration at 1.25x speed:\n" + secondsTo(playlist_duration // 1.25))
-            flash("Duration at 1.5 speed:\n" + secondsTo(playlist_duration // 1.5))
+           # flash("Duration at 1.25x speed:\n" + secondsTo(playlist_duration // 1.25))
+           # flash("Duration at 1.5 speed:\n" + secondsTo(playlist_duration // 1.5))
             flash("Total Playlist Views: " + str(p.views))
             flash("Number of Videos: " + str(p.length))
             flash("Last updated: "+ str(p.last_updated))
