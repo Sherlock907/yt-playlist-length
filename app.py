a7c4724b135d908b6af8e914a71c5ca5feb9ee29
Sherlock907 <106328395+Sherlock907@users.noreply.github.com>
2022-07-15 16:47:28 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import os
 
 
 def getseconds(n):
+    #decrypting the YT-API Timeformat
     n, temp, duration = n.replace("PT", ""), "", 0
-
     for c in n:
         if c.isnumeric():
             temp += c
@@ -55,22 +55,26 @@ def getlength(p):
     api_key = os.getenv('API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
 
-    # get videoID with pytube as youtube API only gets you the first 50 with workaround 100...
+    # get videoID with pytube as youtube API only gets you the first 50-100 IDs...
     vid_ids = [YouTube(url).video_id for url in p]
 
     # total duration, number of videos, divisor for 50 (youtubes api only allows to request information for 50 videos at once
     duration, vid_count, div = 0, len(vid_ids), len(vid_ids)//50
 
-    if div != 0 or vid_count < 50:
-        vid_request = youtube.videos().list(
-            part="contentDetails",
-            id=','.join(vid_ids[50 * div:50 * div + vid_count % 50])
-        )
-        vid_response = vid_request.execute()
+    # gets vid length from videos <50 or those which over lap 50/100
+    # for example in a playlist with 103 videos, the code below gets the duration of video 101-103
+    vid_request = youtube.videos().list(
+        part="contentDetails",
+        id=','.join(vid_ids[50 * div:50 * div + vid_count % 50])
+    )
+    vid_response = vid_request.execute()
 
-        for item in vid_response ['items']:
-            duration += getseconds(item['contentDetails']['duration'])
+    # adds the duration from the api response
+    for item in vid_response ['items']:
+        duration += getseconds(item['contentDetails']['duration'])
 
+
+    # get videos from positions diviseable by 50 -> (0:50, 50:100, ..)
     for i in range(0,div):
         vid_request = youtube.videos().list(
             part="contentDetails",
@@ -92,19 +96,6 @@ app.secret_key = "SpoilerAlert"
 def index():
     if request.method == "POST":
         try:
-            if (x:=str(request.form['yt-link']).lower()) == "bautz" or sorted(x) == sorted("bautz") :
-                flash("Sie suchen also nach dem großen Lebautzski")
-                flash(Markup("Hier werden Sie fündig:  <a href='https://youtu.be/9cK3QYZ7XaA'>The Big Lebautzski</a>"))
-                return render_template('index.html')
-            elif x == "mariya":
-                flash(Markup("<h5 style='padding-bottom: 15px;text-decoration:underline;'>Thank you for your insightful tutorials, Mariya.</h5>"))
-                return render_template('index.html')
-            elif sorted(x) == sorted("mariya"):
-                flash("Having a hard time writing your own name? - No Problem.")
-                flash(Markup('<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>'))
-                flash(Markup('''<h1 class="ml2">It's Mariya</h1>'''))
-                flash(Markup('''<script>var textWrapper=document.querySelector('.ml2');textWrapper.innerHTML=textWrapper.textContent.replace(/\S/g,"<span class='letter'>$&</span>");anime.timeline({loop:true}).add({targets:'.ml2 .letter',opacity:[0,0],translateZ:0,easing:"easeOutExpo",duration:1750,delay:(el,i)=>70*i}).add({targets:'.ml2 .letter',scale:[4,1],opacity:[0,1],translateZ:0,easing:"easeOutExpo",duration:2550,delay:(el,i)=>700*i}).add({targets:'.ml2',opacity:0,duration:1000,easing:"easeOutExpo",delay:1000});</script>'''))
-                return render_template('index.html')
             link = request.form['yt-link']
             p = Playlist(link)
             flash(Markup("<a href='https://www.youtube.com/playlist?list=" + p.playlist_id + "'>" + p.title + "</a>"))
@@ -114,7 +105,7 @@ def index():
             try:
                 flash("Last updated: "+ str(p.last_updated))
             except:
-                flash("Last updated: probably Today :)")
+                flash("Last updated: N/A")
             return render_template('yt-playlist.html')
         except:
             flash("Whoops!.. Something went wrong.")
